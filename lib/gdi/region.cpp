bf511a8c4fc8a25a6c0b14ef0a9e4112d0c143c0
vuteam <rakzami39@gmail.com>
2016-11-01 16:13:02 +0100

    Update region.cpp

--- lib/gdi/region.cpp
+++ lib/gdi/region.cpp
@@ -282,7 +282,7 @@ void gRegion::regionOp(const gRegion &reg1, const gRegion &reg2, int opcode, int
 				}
 			}
 			        ytop = r1y1;
-		      } else { 
+		      } else  
 			        ytop = r1y1;
 			        ybot = min(r1->y2, r2->y2);
 		if (ybot > ytop) {
